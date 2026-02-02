b43c1d736dd6a7757e2062bec9cce17a5b3b954c
Ivana Hrivnacova <Ivana.Hrivnacova@cern.ch>
2026-02-02 17:56:37 +0100

    rebuilding site Mon Feb  2 17:56:37 CET 2026

--- js/search.js
+++ js/search.js
@@ -1,96 +1,214 @@
-var lunrIndex, pagesIndex;
+(function () {
+  function ready(fn) {
+    if (document.readyState == 'complete') {
+      fn();
+    } else {
+      document.addEventListener('DOMContentLoaded', fn);
+    }
+  }
 
-function endsWith(str, suffix) {
-    return str.indexOf(suffix, str.length - suffix.length) !== -1;
-}
+  function escapeRegex(string) {
+    return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+  }
 
-// Initialize lunrjs using our generated index file
-function initLunr() {
-    if (!endsWith(baseurl,"/")){
-        baseurl = baseurl+'/'
-    };
+  window.relearn = window.relearn || {};
 
-    // First retrieve the index file
-    $.getJSON(baseurl +"index.json")
-        .done(function(index) {
-            pagesIndex = index;
-            // Set up lunrjs by declaring the fields we use
-            // Also provide their boost level for the ranking
-            lunrIndex = lunr(function() {
-                this.ref("uri");
-                this.field('title', {
-		    boost: 15
-                });
-                this.field('tags', {
-		    boost: 10
-                });
-                this.field("content", {
-		    boost: 5
-                });
-				
-                this.pipeline.remove(lunr.stemmer);
-                this.searchPipeline.remove(lunr.stemmer);
-				
-                // Feed lunr with each file and let lunr actually index them
-                pagesIndex.forEach(function(page) {
-		    this.add(page);
-                }, this);
-            })
-        })
-        .fail(function(jqxhr, textStatus, error) {
-            var err = textStatus + ", " + error;
-            console.error("Error getting Hugo index file:", err);
-        });
-}
+  window.relearn.executeInitialSearch = function executeInitialSearch() {
+    if (window.relearn.isSearchInterfaceReady && window.relearn.isSearchEngineReady) {
+      var input = document.querySelector('#R-search-by-detail');
+      if (!input) {
+        return;
+      }
+      var value = input.value;
+      executeSearch(value);
+    }
+  };
 
-/**
- * Trigger a search in lunr and transform the result
- *
- * @param  {String} query
- * @return {Array}  results
- */
-function search(queryTerm) {
-    // Find the item in our index corresponding to the lunr one to have more info
-    return lunrIndex.search(queryTerm+"^100"+" "+queryTerm+"*^10"+" "+"*"+queryTerm+"^10"+" "+queryTerm+"~2^1").map(function(result) {
-            return pagesIndex.filter(function(page) {
-                return page.uri === result.ref;
-            })[0];
-        });
-}
+  window.relearn.executeTriggeredSearch = function executeTriggeredSearch() {
+    var input = document.querySelector('#R-search-by-detail');
+    if (!input) {
+      return;
+    }
+    var value = input.value;
+    executeSearch(value);
 
-// Let's get started
-initLunr();
-$( document ).ready(function() {
-    var searchList = new autoComplete({
-        /* selector for the search box element */
-        selector: $("#search-by").get(0),
-        /* source is the callback to perform the search */
-        source: function(term, response) {
-            response(search(term));
-        },
-        /* renderItem displays individual search results */
-        renderItem: function(item, term) {
-            var numContextWords = 2;
-            var text = item.content.match(
-                "(?:\\s?(?:[\\w]+)\\s?){0,"+numContextWords+"}" +
-                    term+"(?:\\s?(?:[\\w]+)\\s?){0,"+numContextWords+"}");
-            item.context = text;
-            var divcontext = document.createElement("div");
-            divcontext.className = "context";
-            divcontext.innerText = (item.context || '');
-            var divsuggestion = document.createElement("div");
-            divsuggestion.className = "autocomplete-suggestion";
-            divsuggestion.setAttribute("data-term", term);
-            divsuggestion.setAttribute("data-title", item.title);
-            divsuggestion.setAttribute("data-uri", item.uri);
-            divsuggestion.setAttribute("data-context", item.context);
-            divsuggestion.innerText = '» ' + item.title;
-            divsuggestion.appendChild(divcontext);
-            return divsuggestion.outerHTML;
-        },
-        /* onSelect callback fires when a search suggestion is chosen */
-        onSelect: function(e, term, item) {
-            location.href = item.getAttribute('data-uri');
+    // add a new entry to the history after the user
+    // changed the term; this does not reload the page
+    // but will add to the history and update the address bar URL
+    var url = new URL(window.location);
+    var oldValue = url.searchParams.get('search-by');
+    if (value != oldValue) {
+      var state = window.history.state || {};
+      state = Object.assign({}, typeof state === 'object' ? state : {});
+      url.searchParams.set('search-by', value);
+      state.search = url.toString();
+      // with normal pages, this is handled by the 'pagehide' event, but this
+      // doesn't fire in case of pushState, so we have to do the same thing
+      // here, too
+      state.contentScrollTop = +elc.scrollTop;
+      window.history.pushState(state, '', url);
+    }
+  };
+
+  function executeHistorySearch(event) {
+    // restart search if browsed through history
+    if (event.state) {
+      var state = window.history.state || {};
+      state = Object.assign({}, typeof state === 'object' ? state : {});
+      if (state.search) {
+        var url = new URL(state.search);
+        if (url.searchParams.has('search-by')) {
+          var search = url.searchParams.get('search-by');
+
+          // we have to insert the old search term into the inputs
+          var inputs = document.querySelectorAll('input.search-by');
+          inputs.forEach(function (e) {
+            e.value = search;
+            var event = document.createEvent('Event');
+            event.initEvent('input', false, false);
+            e.dispatchEvent(event);
+          });
+
+          // recreate the last search results and eventually
+          // restore the previous scrolling position
+          executeSearch(search);
         }
+      }
+    }
+  }
+
+  function renderItem(item) {
+    var page = item.page;
+    var context = [];
+    if (item.matches && item.matches.length) {
+      var numContextWords = 2;
+      var contextPattern = '(?:\\S+ +){0,' + numContextWords + '}\\S*\\b(?:' + escapeRegex(item.matches[0]) + ')\\b\\S*(?: +\\S+){0,' + numContextWords + '}';
+      context = page.content.match(new RegExp(contextPattern, 'i'));
+      if (!context) {
+        item.matches.shift();
+        var contextPattern = '(?:\\S+ +){0,' + numContextWords + '}\\S*\\b(?:' + item.matches.map(escapeRegex).join('|') + ')\\b\\S*(?: +\\S+){0,' + numContextWords + '}';
+        context = page.content.match(new RegExp(contextPattern, 'i'));
+      }
+    }
+    var divsuggestion = document.createElement('a');
+    divsuggestion.className = 'autocomplete-suggestion';
+    divsuggestion.setAttribute('href', window.relearn.relBaseUri + page.uri);
+    var divtitle = document.createElement('div');
+    divtitle.className = 'title';
+    divtitle.innerText = '» ' + page.title;
+    divsuggestion.appendChild(divtitle);
+    var divbreadcrumb = document.createElement('div');
+    divbreadcrumb.className = 'breadcrumbs';
+    divbreadcrumb.innerText = page.breadcrumb || '';
+    divsuggestion.appendChild(divbreadcrumb);
+    if (context) {
+      var divcontext = document.createElement('div');
+      divcontext.className = 'context';
+      divcontext.innerText = context || '';
+      divsuggestion.appendChild(divcontext);
+    }
+    return divsuggestion;
+  }
+
+  function executeSearch(value) {
+    var input = document.querySelector('#R-search-by-detail');
+    function resolvePlaceholders(s, args) {
+      var args = args || [];
+      // use replace to iterate over the string
+      // select the match and check if the related argument is present
+      // if yes, replace the match with the argument
+      return s.replace(/{([0-9]+)}/g, function (match, index) {
+        // check if the argument is present
+        return typeof args[index] == 'undefined' ? match : args[index];
+      });
+    }
+
+    var results = document.querySelector('#R-searchresults');
+    var hint = document.querySelector('.searchhint');
+    hint.innerText = '';
+    results.textContent = '';
+    (async function () {
+      var a = await window.relearn.search.adapter.search(value);
+      if (a.length) {
+        hint.innerText = resolvePlaceholders(window.T_N_results_found, [value, a.length]);
+        a.forEach(function (item) {
+          results.appendChild(renderItem(item));
+        });
+        window.relearn.markSearch();
+      } else if (value.length) {
+        hint.innerText = resolvePlaceholders(window.T_No_results_found, [value]);
+      }
+      input.focus();
+      setTimeout(adjustContentWidth, 0);
+
+      // if we are initiating search because of a browser history
+      // operation, we have to restore the scrolling postion the
+      // user previously has used; if this search isn't initiated
+      // by a browser history operation, it simply does nothing
+      var state = window.history.state || {};
+      state = Object.assign({}, typeof state === 'object' ? state : {});
+      if (state.hasOwnProperty('contentScrollTop')) {
+        window.setTimeout(function () {
+          elc.scrollTop = +state.contentScrollTop;
+        }, 10);
+        return;
+      }
+    })();
+  }
+
+  function initSearchAfterLoad() {
+    if (!window.relearn.search || !window.relearn.search.adapter) {
+      return;
+    }
+    window.relearn.search.adapter.init();
+
+    var input = document.querySelector('#R-search-by-detail');
+    if (input) {
+      var state = window.history.state || {};
+      state = Object.assign({}, typeof state === 'object' ? state : {});
+      state.search = window.location.toString();
+      window.history.replaceState(state, '');
+    }
+
+    new autoComplete({
+      /* selector for the search box element */
+      selectorToInsert: 'search:has(.searchbox)',
+      selector: '#R-search-by',
+      /* source is the callback to perform the search */
+      source: async function (term, response) {
+        let a = await window.relearn.search.adapter.search(term);
+        response(a);
+      },
+      /* renderItem displays individual search results */
+      renderItem: function (item, _term) {
+        return renderItem(item).outerHTML;
+      },
+      /* onSelect callback fires when a search suggestion is chosen */
+      onSelect: function (e, term, item) {
+        location.href = item.getAttribute('href');
+        e.preventDefault();
+      },
     });
-});
+  }
+
+  function initSearch() {
+    window.addEventListener('popstate', executeHistorySearch);
+
+    var input = document.querySelector('#R-search-by-detail');
+    if (input) {
+      input.addEventListener('keydown', function (event) {
+        // if we are pressing ESC in the searchdetail our focus will
+        // be stolen by the other event handlers, so we have to refocus
+        // here after a short while
+        if (event.key == 'Escape') {
+          setTimeout(function () {
+            input.focus();
+          }, 0);
+        }
+      });
+    }
+    ready(initSearchAfterLoad);
+  }
+
+  initSearch();
+})();
